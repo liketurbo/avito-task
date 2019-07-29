2c8d5b01644ee78ebfec78059ea9b0b5a112ed0e
Ramzan Chasygov <theliketurbo@gmail.com>
2019-07-29 22:41:04 +0300

    Added main page

---
+++ src/pages/main.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+import Card, { ICard } from '../components/Card';
+import Grid from '../components/Grid';
+import { ISeller } from '../components/Seller';
+import useFetch from '../hooks/useFetch';
+
+export default () => {
+  const [combined, setCombined] = useState<any>([]);
+
+  const { data: products } = useFetch<ICard[]>(
+    'https://avito.dump.academy/products'
+  );
+  const { data: sellers } = useFetch<ISeller[]>(
+    'https://avito.dump.academy/sellers'
+  );
+
+  useEffect(() => {
+    if (products && sellers) {
+      setCombined(
+        products.map(product => {
+          const sellerId = product.relationships.seller;
+          const seller = sellers.find(({ id }) => id === sellerId);
+
+          return { ...product, seller };
+        })
+      );
+    }
+  }, [products, sellers]);
+
+  return (
+    <Grid>
+      {combined.length &&
+        combined
+          .slice(1, 5)
+          .map((product: any) => <Card key={product.id} {...product} />)}
+    </Grid>
+  );
+};
